52865aeecca6d7880b28254701afbcda11539456
Anonymous <anonymous@noemail.com>
2016-05-07 00:03:54 +0000

    init

---
+++ main/java/com/codenvy/example/java/Calculadora.java
@@ -0,0 +1,42 @@
+package br.com.calculapolonesa.grupo3;
+
+public class Calculadora {
+  
+  private double valorVisor;
+  private Pilha pilha;
+  
+  
+  public void calcular (int calcular){
+
+
+  }
+  
+  public double somar(double primeiroDigito, double segundoDigito){
+    return 0;
+  }
+  
+  public double subtrair(double primeiroDigito, double segundoDigito){
+    return 0;
+  }
+  
+  public double multiplicar(double primeiroDigito, double segundoDigito){
+    return 0;
+  }
+  
+  public double dividir(double primeiroDigito, double segundoDigito){
+    
+    return 0;
+  }
+  
+  
+  public double obterValorVisor(){
+    
+    return 0;
+  }
+  
+  public void preencherValorVisor(double valor){
+    
+  }
+  
+  
+}
---
+++ main/java/com/codenvy/example/java/Pilha.java
@@ -0,0 +1,25 @@
+package br.com.calculapolonesa.grupo3;
+import java.util.*;
+
+public class Pilha {
+  
+  
+  private List <double> pilha = new ArrayList<double>();
+  
+  public boolean empilhar (double digito) {
+    
+    
+  }
+  
+  
+  public boolean desempilhar (double resultado) {
+    
+    
+  }
+  
+  public boolean limparVetor () {
+    
+    
+  }
+  
+}
